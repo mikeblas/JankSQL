34f6afd4a8977be9755ef4177bbbb732ca1d1b2f
Mike Blaszczak <mikeblas@gmail.com>
2022-03-04 18:59:08 -0800

    flesh out FullTableName structure

--- JankSQL/FullTableName.cs
+++ JankSQL/FullTableName.cs
@@ -11,7 +11,74 @@ namespace JankSQL
         string? linkedServerName;
         string? schemaName;
         string? databaseName;
-        string? tableName;
+        string tableName;
+
+        internal static FullTableName FromTableNameContext(TSqlParser.Table_nameContext context)
+        {
+            var r = new FullTableName(Program.GetEffectiveName(context.table.GetText()));
+            r.databaseName = (context.database != null) ? Program.GetEffectiveName(context.database.GetText()) : null;
+            r.schemaName = (context.schema != null) ? Program.GetEffectiveName(context.schema.GetText()) : null;
+            return r;
+        }
+
+        internal static FullTableName FromFullTableNameContext(TSqlParser.Full_table_nameContext context)
+        {
+            var r = new FullTableName(Program.GetEffectiveName(context.table.GetText()));
+            r.databaseName = (context.database != null) ? Program.GetEffectiveName(context.database.GetText()) : null;
+            r.schemaName = (context.schema != null) ? Program.GetEffectiveName(context.schema.GetText()) : null;
+            r.linkedServerName = (context.linkedServer != null) ? Program.GetEffectiveName(context.schema.GetText()) : null;
+            return r;
+        }
+
+        FullTableName(string tableName)
+        {
+            this.tableName = tableName;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 19;
+
+            if (linkedServerName != null)
+                hash = hash * 31 + linkedServerName.GetHashCode();
+            if (databaseName != null)
+                hash = hash * 31 + databaseName.GetHashCode();
+            if (schemaName != null)
+                hash = hash * 31 + schemaName.GetHashCode();
+            if (tableName != null)
+                hash = hash * 31 + tableName.GetHashCode();
+
+            return hash;
+        }
+
+        public override string ToString()
+        {
+            //REVIEW: is this right? could be that serverName != null but schemaName == null, and ...
+            string ret = "";
+            if (linkedServerName != null)
+                ret += $"[{linkedServerName}]";
+
+            if (databaseName != null)
+            {
+                if (ret.Length > 0)
+                    ret += ".";
+                ret += $"[{databaseName}]";
+            }
+            if (schemaName != null)
+            {
+                if (ret.Length > 0)
+                    ret += ".";
+                ret += $"[{schemaName}]";
+            }
+            if (tableName != null)
+            {
+                if (ret.Length > 0)
+                    ret += ".";
+                ret += $"[{tableName}]";
+            }
+
+            return ret;
+        }
 
     }
 }
