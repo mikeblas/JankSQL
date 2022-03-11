8d6cb551bb2db78202b391ed3d50fb6a3f464676
Mike Blaszczak <mikeblas@gmail.com>
2022-03-11 14:56:16 -0800

    prepared to implement UPDATE

--- JankSQL/Listeners/UpdateListener.cs
+++ JankSQL/Listeners/UpdateListener.cs
@@ -15,8 +15,32 @@ namespace JankSQL
             updateContext = new UpdateContext(context, FullTableName.FromFullTableNameContext(context.ddl_object().full_table_name()));
             Console.WriteLine($"UPDATE {updateContext.TableName}");
 
+            Expression pred = GobbleSearchCondition(context.search_condition());
             predicateContext = new();
+            predicateContext.EndPredicateExpressionList(pred);
 
+            foreach (var element in context.update_elem())
+            {
+                FullColumnName fcn = FullColumnName.FromContext(element.full_column_name());
+                Expression x = GobbleExpression(element.expression());
+
+                Console.Write($"   SET {fcn} ");
+                if (element.Equals != null)
+                {
+                    Console.Write("Equals ");
+                    updateContext.AddAssignment(fcn, x);
+                }
+                else if (element.assignment_operator() != null)
+                {
+                    Console.Write("Assignment operator {element.assignment_operator().GetText()}");
+                    updateContext.AddAssignmentOperator(fcn, element.assignment_operator().GetText(), x);
+                }
+                else
+                {
+                    throw new InvalidOperationException("unknown UPDATE structure");
+                }
+                Console.WriteLine($"{x}");
+            }
         }
 
         public override void EnterUpdate_elem([NotNull] TSqlParser.Update_elemContext context)
--- JankSQL/UpdateContext.cs
+++ JankSQL/UpdateContext.cs
@@ -1,10 +1,42 @@
 ﻿
 namespace JankSQL
 {
+    enum SetOperator
+    {
+        ASSIGN,
+        ADD_ASSIGN,
+        SUB_ASSIGN,
+        MUL_ASSIGN,
+        DIV_ASSIGN,
+        MOD_ASSIGN,
+    }
+
+    internal class SetOperation
+    {
+        FullColumnName fcn;
+        Expression expression;
+        SetOperator op;
+
+        internal SetOperation(FullColumnName fcn, SetOperator op, Expression expression)
+        {
+            this.fcn = fcn;
+            this.op = op;
+            this.expression = expression;
+        }
+
+        public override string ToString()
+        {
+            string s = $"{fcn} {op} {expression}";
+            return s;
+        }
+    }
+
     internal class UpdateContext : IExecutableContext
     {
         FullTableName tableName;
         TSqlParser.Update_statementContext context;
+
+        List<SetOperation> setList = new();
 
         internal UpdateContext(TSqlParser.Update_statementContext context, FullTableName tableName)
         {
@@ -16,19 +48,44 @@ namespace JankSQL
 
         internal FullTableName TableName { get { return tableName; } }
 
+        internal void AddAssignment(FullColumnName fcn, Expression x)
+        {
+            SetOperation op = new SetOperation(fcn, SetOperator.ASSIGN, x);
+            setList.Add(op);
+        }
+        
+        internal void AddAssignmentOperator(FullColumnName fcn, string op, Expression x)
+        {
+            throw new NotImplementedException();
+        }
+
         public void Dump()
         {
             Console.WriteLine($"UPDATE {tableName}");
 
+            Console.WriteLine("   Predicates:");
             if (PredicateContext == null || PredicateContext.PredicateExpressionListCount == 0)
             {
-                Console.WriteLine("   no predicates");
+                Console.WriteLine("      no predicates");
             }
             else
             {
                 for (int i = 0; i < PredicateContext.PredicateExpressionListCount; i++)
                 {
-                    Console.WriteLine($"    {PredicateContext.PredicateExpressions[i]}");
+                    Console.WriteLine($"       {PredicateContext.PredicateExpressions[i]}");
+                }
+            }
+
+            Console.WriteLine("   Assignments:");
+            if (setList == null || setList.Count == 0)
+            {
+                Console.WriteLine("   no assignments");
+            }
+            else
+            {
+                foreach (var op in setList)
+                {
+                    Console.WriteLine($"      {op}");
                 }
             }
         }
