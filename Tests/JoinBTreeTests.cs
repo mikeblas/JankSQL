c9d6d7e3af9bfcd2ee2b7238cd28b41cc6fb03b5
Mike Blaszczak <mikeblas@gmail.com>
2022-03-15 20:09:58 -0700

    hook up BTree join tests

---
+++ Tests/JoinBTreeTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Engines = JankSQL.Engines;
+using System.IO;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class JoinCBTreeTests : JoinTests
+    {
+        [TestInitialize]
+        public void ClassInitialize()
+        {
+            mode = "BTree";
+            Console.WriteLine($"Test mode is {mode}");
+
+            engine = Engines.BTreeEngine.CreateInMemory();
+            TestHelpers.InjectTableMyTable(engine);
+
+            TestHelpers.InjectTableTen(engine);
+
+            TestHelpers.InjectTableStates(engine);
+
+            TestHelpers.InjectTableThree(engine);
+        }
+    }
+}
