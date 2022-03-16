ce939f152185e3746006e4d1585593f46e53bbd2
Mike Blaszczak <mikeblas@gmail.com>
2022-03-16 15:22:47 -0700

    insert/delete tests for BTree too

---
+++ Tests/InsertDeleteBTreeTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Engines = JankSQL.Engines;
+using System.IO;
+
+namespace Tests
+{
+    [TestClass]
+    public class InsertDeleteBTreeTests : InsertDeleteTests
+    {
+        [TestInitialize]
+        public void ClassInitialize()
+        {
+            mode = "BTree";
+
+            string tempPath = Path.GetTempPath();
+            engine = Engines.BTreeEngine.CreateInMemory();
+
+            TestHelpers.InjectTableMyTable(engine);
+        }
+    }
+}
