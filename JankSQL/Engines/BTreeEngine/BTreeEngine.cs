7ae431ad777ab6acbce43717b258d4c7a7cf9123
Mike Blaszczak <mikeblas@gmail.com>
2022-03-19 12:05:38 -0700

    remove come commented code

--- JankSQL/Engines/BTreeEngine/BTreeEngine.cs
+++ JankSQL/Engines/BTreeEngine/BTreeEngine.cs
@@ -27,14 +27,6 @@ namespace JankSQL.Engines
                 throw new ArgumentException($"Must have at types for each column; got {columnNames.Count} names and {columnTypes.Count} types");
 
             // create the table
-            /*
-            BTreeTable table = new BTreeTable(tableName.TableName,
-                new ExpressionOperandType[] { ExpressionOperandType.INTEGER },
-                new FullColumnName[] { FullColumnName.FromColumnName("bookmark_key") }.ToList(),
-                columnTypes.ToArray(),
-                columnNames);
-            */
-
             BTreeTable table = new BTreeTable(tableName.TableName,
                 columnTypes.ToArray(),
                 columnNames);
