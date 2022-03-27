981627ca52049a86588a50d3eba8b8f5c091c007
Mike Blaszczak <mikeblas@gmail.com>
2022-03-27 15:45:48 -0700

    some index tests

--- Tests/EngineIndexTests.cs
+++ Tests/EngineIndexTests.cs
@@ -27,16 +27,97 @@
             var idx = t.Index("evenIndex");
             Assert.IsNotNull(idx);
 
-            foreach (var columnInfo in idx.IndexDefinition.ColumnInfos)
-            {
-                Console.Write($"{columnInfo.columnName}, ");
-            }
-            Console.WriteLine();
+            string s = String.Join(",", idx.IndexDefinition.ColumnInfos.Select(x => $"[{x.columnName}, {(x.isDescending ? "DESC" : "ASC")}]"));
+            Console.WriteLine(s);
 
             foreach (var r in idx)
             {
                 Console.WriteLine($"{r.RowData} ==> {r.Bookmark}");
             }
+
+
+            int oddCount = 0;
+            int evenCount = 0;
+            int polarityIndex = idx.IndexDefinition.ColumnIndex("is_even");
+            foreach (var indexRow in idx)
+            {
+                int p = indexRow.RowData[polarityIndex].AsInteger();
+                if (p == 0)
+                {
+                    oddCount += 1;
+                    Assert.AreEqual(0, evenCount, "Odds must come before any even");
+                }
+                else if (p == 1)
+                {
+                    evenCount += 1;
+                    Assert.AreEqual(5, oddCount, "Evens must come after all odds");
+                }
+                else
+                    Assert.Fail($"Didn't expect is_even value {p}");
+            }
+
+            Assert.AreEqual(5, evenCount);
+            Assert.AreEqual(5, oddCount);
+        }
+
+
+        [TestMethod]
+        public void TestCreateInsertIndex()
+        {
+            // create a non-unique index on a test table
+            List<(string columnName, bool isDescending)> columnInfos = new()
+            {
+                ("is_even", false)
+            };
+
+            engine.CreateIndex(FullTableName.FromTableName("ten"), "evenIndex", false, columnInfos);
+
+            // get our table
+            Engines.IEngineTable? t = engine.GetEngineTable(FullTableName.FromTableName("ten"));
+            Assert.IsNotNull(t);
+
+            Tuple newRow = Tuple.CreateEmpty(4);
+            newRow[0] = ExpressionOperand.IntegerFromInt(11);
+            newRow[1] = ExpressionOperand.NVARCHARFromString("eleven");
+            newRow[2] = ExpressionOperand.IntegerFromInt(0);
+
+            t.InsertRow(newRow);
+
+            var idx = t.Index("evenIndex");
+            Assert.IsNotNull(idx);
+
+            string s = String.Join(",", idx.IndexDefinition.ColumnInfos.Select(x => $"[{x.columnName}, {(x.isDescending ? "DESC" : "ASC")}]"));
+            Console.WriteLine(s);
+
+            foreach (var r in idx)
+            {
+                Console.WriteLine($"{r.RowData} ==> {r.Bookmark}");
+            }
+
+
+            int oddCount = 0;
+            int evenCount = 0;
+            int polarityIndex = idx.IndexDefinition.ColumnIndex("is_even");
+            foreach (var indexRow in idx)
+            {
+                int p = indexRow.RowData[polarityIndex].AsInteger();
+                if (p == 0)
+                {
+                    oddCount += 1;
+                    Assert.AreEqual(0, evenCount, "Odds must come before any even");
+                }
+                else if (p == 1)
+                {
+                    evenCount += 1;
+                    Assert.AreEqual(6, oddCount, "Evens must come after all odds");
+                }
+                else
+                    Assert.Fail($"Didn't expect is_even value {p}");
+            }
+
+            Assert.AreEqual(6, oddCount);
+            Assert.AreEqual(5, evenCount);
         }
     }
 }
+
