0726d2c93ed6adedab982001b0f1ccb312965fb8
Mike Blaszczak <mikeblas@gmail.com>
2022-04-19 19:51:17 -0700

    more tests about aliases

--- Tests/QuestionTests.cs
+++ Tests/QuestionTests.cs
@@ -14,26 +14,7 @@
         [Test]
         public void TestActualStudentGrades()
         {
-            string creates = @"
-CREATE TABLE students (
-    StudentID INTEGER,
-    StudentName VARCHAR(255) NOT NULL,
-    score INTEGER NOT NULL,
-    class INTEGER NOT NULL    
-);
-
-INSERT INTO students(StudentID, StudentName, score, class) VALUES(1, 'Mark', 894, 7);
-INSERT INTO students(StudentID, StudentName, score, class) VALUES(2, 'Bill', 894, 7);
-INSERT INTO students(StudentID, StudentName, score, class) VALUES(3, 'Maria', 678, 8);
-INSERT INTO students(StudentID, StudentName, score, class) VALUES(4, 'David', 733, 9);
-INSERT INTO students(StudentID, StudentName, score, class) VALUES(5, 'John', 899, 9);
-INSERT INTO students(StudentID, StudentName, score, class) VALUES(6, 'Rob', 802, 9);";
-
-
-            var ecCreate = Parser.ParseSQLFileFromString(creates);
-
-            ExecuteResult resultCreate = ecCreate.ExecuteSingle(engine);
-            JankAssert.SuccessfulWithMessageNoResultSet(resultCreate);
+            CreateStudentsTable();
 
             string select =
                 "SELECT Students.StudentName, Students.Score, Students.Class "+
@@ -46,12 +27,87 @@ INSERT INTO students(StudentID, StudentName, score, class) VALUES(6, 'Rob', 802,
             ExecuteResult resultSelect = ecSelect.ExecuteSingle(engine);
 
             resultSelect.ResultSet.Dump();
-
         }
 
 
         [Test]
         public void TestActualStudentGradesAliases()
+        {
+            CreateStudentsTable();
+
+            string select =
+                "SELECT Y.StudentName, Y.Score, Y.Class " +
+                "  FROM Students Y" +
+                "  JOIN ( SELECT Class, MAX(Score) TopScore FROM Students GROUP BY Class) X " +
+                "    ON X.Class = Y.Class AND X.TopScore = Y.Score; ";
+
+            var ecSelect = Parser.ParseSQLFileFromString(select);
+
+            ExecuteResult resultSelect = ecSelect.ExecuteSingle(engine);
+
+            resultSelect.ResultSet.Dump();
+
+        }
+
+
+        [Test]
+        public void TestActualStudentGradesInnerAliases()
+        {
+            CreateStudentsTable();
+
+            string select =
+                "SELECT Students.StudentName, Students.Score, Students.Class " +
+                "  FROM Students " +
+                "  JOIN ( SELECT Class, MAX(Score) TopScore FROM Students GROUP BY Class) X " +
+                "    ON X.Class = Students.Class AND X.TopScore = Students.Score; ";
+
+            var ecSelect = Parser.ParseSQLFileFromString(select);
+
+            ExecuteResult resultSelect = ecSelect.ExecuteSingle(engine);
+
+            resultSelect.ResultSet.Dump();
+        }
+
+        [Test]
+        public void TestActualStudentGradesInnerAggAliases()
+        {
+            CreateStudentsTable();
+
+            string select =
+                "SELECT Y.StudentName, Y.Score, Y.Class " +
+                "  FROM Students Y" +
+                "  JOIN ( SELECT XX.Class, MAX(XX.Score) TopScore FROM Students AS XX GROUP BY XX.Class) X " +
+                "    ON X.Class = Y.Class AND X.TopScore = Y.Score; ";
+
+            var ecSelect = Parser.ParseSQLFileFromString(select);
+
+            ExecuteResult resultSelect = ecSelect.ExecuteSingle(engine);
+
+            resultSelect.ResultSet.Dump();
+
+        }
+
+        [Test]
+        public void TestActualStudentGradesInnerAggAliasDefault()
+        {
+            CreateStudentsTable();
+
+            string select =
+                "SELECT Y.StudentName, Y.Score, Y.Class " +
+                "  FROM Students Y" +
+                "  JOIN ( SELECT Class, MAX(Score) TopScore FROM Students AS XX GROUP BY Class) X " +
+                "    ON X.Class = Y.Class AND X.TopScore = Y.Score; ";
+
+            var ecSelect = Parser.ParseSQLFileFromString(select);
+
+            ExecuteResult resultSelect = ecSelect.ExecuteSingle(engine);
+
+            resultSelect.ResultSet.Dump();
+
+        }
+
+
+        private void CreateStudentsTable()
         {
             string creates = @"
 CREATE TABLE students (
@@ -73,20 +129,6 @@ INSERT INTO students(StudentID, StudentName, score, class) VALUES(6, 'Rob', 802,
 
             ExecuteResult resultCreate = ecCreate.ExecuteSingle(engine);
             JankAssert.SuccessfulWithMessageNoResultSet(resultCreate);
-
-            string select =
-                "SELECT Y.StudentName, Y.Score, Y.Class " +
-                "  FROM Students Y" +
-                "  JOIN ( SELECT Class, MAX(Score) TopScore FROM Students GROUP BY Class) X " +
-                "    ON X.Class = Y.Class AND X.TopScore = Y.Score; ";
-
-            var ecSelect = Parser.ParseSQLFileFromString(select);
-
-            ExecuteResult resultSelect = ecSelect.ExecuteSingle(engine);
-
-            resultSelect.ResultSet.Dump();
-
         }
-
     }
 }
