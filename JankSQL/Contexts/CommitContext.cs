e73bc6d200fadae3caf76b31e1d65a573987a43a
Mike Blaszczak <mikeblas@gmail.com>
2022-04-22 20:32:34 -0700

    COMMIT and ROLLBACK transaction controls

---
+++ JankSQL/Contexts/CommitContext.cs
@@ -0,0 +1,26 @@
+﻿using JankSQL.Engines;
+using JankSQL.Expressions;
+
+namespace JankSQL.Contexts
+{
+    internal class CommitContext : IExecutableContext
+    {
+        public object Clone()
+        {
+            return new CommitContext();
+        }
+
+        public void Dump()
+        {
+            Console.WriteLine("CommitContext");
+        }
+
+        public ExecuteResult Execute(IEngine engine, IRowValueAccessor? accessor, Dictionary<string, ExpressionOperand> bindValues)
+        {
+            // engine.Rollback();
+
+            ExecuteResult results = ExecuteResult.SuccessWithMessage("Committed");
+            return results;
+        }
+    }
+}
